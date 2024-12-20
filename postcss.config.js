bfbff8eb261538795ea41f901d6f7cc11a8a7bcb
aglamkhujaev <aflamkhujaev@gmail.com>
2024-12-20 07:29:59 +0500

    feat: add basic files and configurations

---
+++ postcss.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    theme: {
+      extend: {
+        colors: {
+          primary: 'red', // umumiy rang
+          secondary: '#F59E0B', // ikkinchi rang
+        },
+        fontSize: {
+          sm: '0.875rem',  // kichik font
+          base: '1rem',     // asosiy font
+          lg: '1.125rem',   // kattaroq font
+        },
+        fontWeight: {
+          regular: '400',
+          medium: '500',
+          bold: '700',
+        },
+      },
+    },
+  }
+  
