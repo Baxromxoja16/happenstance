e0922364020db55f0d7ceff0594edc5b8ffdc9b1
aglamkhujaev <aflamkhujaev@gmail.com>
2024-12-21 14:45:03 +0500

    fix: router change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ProductViewerComponent } from './product-viewer/product-viewer.component.js';
 import { HeaderComponent } from './components/shared/header/header.component.js';
 
 @Component({
   selector: 'app-root',
-  imports: [ProductViewerComponent, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,6 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'product-detail', pathMatch: 'full' },
-    { path: 'product-detail', loadComponent: () => import('./components/product/product-details/product-details.component').then(m => m.ProductDetailsComponent) }, // Used Lazy loading for this route
+    { path: 'product-detail', loadComponent: () => import('./product-viewer/product-viewer.component').then(m => m.ProductViewerComponent) }, // Used Lazy loading for this route
     { path: '**', redirectTo: 'product-detail' } // Wildcard route for a 404 page
 ];
