9277991b9baf9fae1c3f96c795673691ac4debb2
aglamkhujaev <aflamkhujaev@gmail.com>
2024-12-22 07:45:47 +0500

    fix: image change

--- src/db/db.ts
+++ src/db/db.ts
@@ -8,13 +8,13 @@ export default {
         {
           id: 1,
           name: 'HUNK',
-          color: '#8b5855',
-          colorName: 'RED',
+          color: 'brown',
+          colorName: 'Brown',
           price: 1599,
           image: [
-            'https://yandex-images.clstorage.net/WWw5g4164/75622cgQ/dguedAkSFH-jKYxQX6edOa-aBT3agfh9-hFcyrRfh9AgJpYL-KUZskW8LoTnoyEl1iRTv9UA0CmFFjXp7FloMdM_qzAMJqGb0yndFO8DXJlum1H8DrBZWHrSP312DfE3VnExKX8UqefgmS8QM6Iys2dHN7qT88QrogNWXY8IrDfXeu_3G8I1ihD5bxfNdV0avTrDDfgxvE5dgJwvRW3B5yRQAEtNZUmkoevcRVJyEBHxpJaIEMLnG1MAdO5_ap_kVvhM9QhjZ4rgWJ_1zmd_qM-osV2qM4293bBurCb_Mdc0UXM4zeeqpeK43fWh0HEiQFcGqLEjperjgNUP_psf5-U5vvZYEmd4ILsOJf9mnHttiRP9KsH9vB_Drn7EXxGH1QHBu43FSWai6U-EQ7NRo4AU1ujTkDUagkNE_k-bL4anOd7ES-HFuELJ7PcdFX1Zv1rC_cvQbS18k6y8ZW8hVKQAwattFFuUIBrt5ZBxA4MxpLSqA-PU6xBDJa2viR3V1mhddOqTpEthGs8WT2avOK664v6Z0b7cTRF_3ERucVbFsdB7rFfY9jG7bGdTsmFTo1V3q9LiFjjSg1a8HQqf5AbIjIX58qX7Edj_1R1E7tju-fJP2VHcDX9TH-2VPtIUBDDT6P6lKqYyWYxGc7AzcLJ3RLpDIdZqQLAmj6-5LpfE-m4Fq7C3iEKr36ZOpN67zNlDX5gzPF_e427-Nj7C1WbxIvjuxbklIvu85-JDIXOxFyTqs5AmSsKDNQ5-ix-lFihfRMgQNetju9_Ennefm59ps-wLgowtzbJNHzSewpalgeMbHtdJd0C6__biQAORgYXmOaPABgsi8FWN_8vO1mdJvsQLkEZ6UeseZpy3ncsO6VFv-nLfDA5gT7zHfwHXtfLTeO3mG7QzyuwWIiAh4rMVB9tzImT601EW7Jw7P3ZGab_myxBHGTNr_dXM9AyLfmsiz3oDzc4-ovwt907Tw', 
-            'http://allstarshoe.ru/wa-data/public/shop/products/55/17/1755/images/12267/12267.970.jpg', 
-            'http://allstarshoe.ru/wa-data/public/shop/products/55/17/1755/images/12263/12263.970.jpg'],
+            'https://happenstance.com/storage/media/D1o7vzY59VJl1YEzVmhfKMsMiS2wOfR55QWgs4vY.webp', 
+            'https://happenstance.com/storage/media/Eu8rWIcts7NWWZN3qn8Bq1Wht01kUvaQI70KnQCz.webp', 
+            'https://happenstance.com/storage/media/7xd8JMvhGqFuIPvJ7g6ttPKrG6Z5psLHNE5Nk1M9.webp'],
           sizes: [
             { us: '8', available: true },
             { us: '8.5', available: true },
@@ -30,9 +30,9 @@ export default {
           colorName: 'BLACK',
           price: 1599,
           image: [
-            'https://wallpapers.com/images/hd/nike-free5-black-running-shoe-hfqd60xo1saxryeg.png',
-            'https://avatars.mds.yandex.net/i?id=36ac47ef5424d7dfd3e2967cb0b94e17_l-5236697-images-thumbs&n=13',
-            'https://images.deal.by/104588822_w640_h640_krossovki-nike-zoom.jpg'
+            'https://happenstance.com/storage/media/N3oDg2pxkchKjTqGmi8Nfuoc9TnvcAMRQCeHCPcM.webp',
+            'https://happenstance.com/storage/media/PodYuT7aESBTD5jWyIkCZvelTE5vC08kE4VHOHNh.webp',
+            'https://happenstance.com/storage/media/Ru1DycfEMrrJ6KOz6l5KDXYq6jr3FAPv1p5L1aKN.webp'
           ],
           sizes: [
             { us: '8', available: true },
@@ -43,13 +43,13 @@ export default {
         {
           id: 3,
           name: 'HUNK',
-          color: '#4169E1',
-          colorName: 'BLUE',
+          color: '#4d2b28',
+          colorName: 'Night Rider',
           price: 1599,
           image:[ 
-            'https://static.cdek.shopping/images/shopping/c94f54b9ae3a4f42807ae6e199bfe79a.png',
-            'https://i.ebayimg.com/images/g/-6cAAOSwV65fXz43/s-l1600.jpg',
-            'https://avatars.mds.yandex.net/i?id=0df2e5101abc545b240394ad66f8ae55e0c9cbbc-5234681-images-thumbs&ref=rim&n=33&w=480&h=200'
+            'https://happenstance.com/storage/media/mgCGcBNdsHY8omirQcxGDUM0pA9sChqFRIfaotov.webp',
+            'https://happenstance.com/storage/media/naemkKzAhQhSH2VOKkessKJqvf0lHM2BugAvz6pj.webp',
+            'https://happenstance.com/storage/media/QN4QFR0NF3Ya3AiVow6WEWneoWbjodtc0uy166Uw.webp'
         ],
           sizes: [
             { us: '7', available: true },
@@ -60,13 +60,13 @@ export default {
         {
           id: 4,
           name: 'HUNK',
-          color: '#1db300',
-          colorName: 'GREEN',
+          color: '#8c8858',
+          colorName: 'Woodland Moss',
           price: 1599,
           image: [
-            'https://wallpapers.com/images/hd/neon-green-nike-air-max-sneaker-72z5fqpgg0dqv7vs.png',
-            'https://i.ebayimg.com/images/g/9ugAAOSwW-Zg388Q/s-l500.jpg',
-            'https://4run.com.ua/wa-data/public/shop/products/06/07/706/images/3093/3093.2000x1500.jpg'
+            'https://happenstance.com/storage/media/fbkrO4DBu3eKYFKYDGRONqDCgAqpOGFPeBTMG5lz.webp',
+            'https://happenstance.com/storage/media/iZsSAyZpGQQOsapxxBHfu3cq5kj5YzujgulOt8Ua.webp',
+            'https://happenstance.com/storage/media/lkLsj5WEPn0YpwUEO2BTqJ1pil2o9niqzHweGkc3.webp'
             ],
           sizes: [
             { us: '8', available: true },
