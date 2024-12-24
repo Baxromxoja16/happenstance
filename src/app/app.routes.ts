9b461c0341e9215002de2d3f6f643e5e94135095
aglamkhujaev <aflamkhujaev@gmail.com>
2024-12-24 09:48:46 +0500

    feat: add guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './core/guards/auth.guard.js';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'login', pathMatch: 'full' },
-    { path: 'product-detail', loadComponent: () => import('./pages/product-viewer/product-viewer.component').then(m => m.ProductViewerComponent) }, // Used Lazy loading for this route
+    { path: 'product-detail', loadComponent: () => import('./pages/product-viewer/product-viewer.component').then(m => m.ProductViewerComponent), canActivate:[authGuard] }, // Used Lazy loading for this route
     { path: 'login', loadComponent: () => import('./pages/login/login.component').then(m => m.LoginComponent) },
     { path: '**', redirectTo: 'login' } // Wildcard route for a 404 page
 ];
