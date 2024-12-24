1d22ea82fd5f1ba8d4e30f8551c864b82a0e7770
aglamkhujaev <aflamkhujaev@gmail.com>
2024-12-24 10:04:05 +0500

    fix: router redirection change

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,8 @@ import { Routes } from '@angular/router';
 import { authGuard } from './core/guards/auth.guard.js';
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'login', pathMatch: 'full' },
+    { path: '', redirectTo: 'product-detail', pathMatch: 'full' },
     { path: 'product-detail', loadComponent: () => import('./pages/product-viewer/product-viewer.component').then(m => m.ProductViewerComponent), canActivate:[authGuard] }, // Used Lazy loading for this route
     { path: 'login', loadComponent: () => import('./pages/login/login.component').then(m => m.LoginComponent) },
-    { path: '**', redirectTo: 'login' } // Wildcard route for a 404 page
+    { path: '**', redirectTo: 'product-detail' } // Wildcard route for a 404 page
 ];
